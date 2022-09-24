4fe2cd3baa6657cd6512a6fbfc3989a7839f36a1
shergin <iamcdgmx@gmail.com>
2022-09-24 22:02:34 +0800

    Initial commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserInput } from './dto/create-user.input';
+import { UpdateUserInput } from './dto/update-user.input';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  create(createUserInput: CreateUserInput) {
+    return 'This action adds a new user';
+  }
+
+  findAll(): User[] {
+    const data = [
+      {
+        id: 1,
+      }
+    ]
+    return data;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserInput: UpdateUserInput) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
